28bd5ee3ad35424ce073697608c741e2590bf4e0
Sarang K <sarangsahajan@gmail.com>
2024-01-24 23:16:00 +0530

    update urlpatterns

--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,9 @@
 # urls.py
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login', views.login, name='login'),  # Add the login URL pattern
-    path('signup', views.signup),  # Allow URL without trailing slash
-    path('test_token', views.protected_view)
+    re_path(r'(?i)^signup\/?', views.signup, name='signup'),
+    re_path(r'(?i)^login\/?', views.login, name='login'),
+    re_path(r'(?i)^test_token\/?', views.protected_view, name='test_token')
 ]
